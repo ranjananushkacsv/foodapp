f7993b6ad90b5e8e8b6e1b45d5ffbdaf1736ed71
ranjananushkacsv <ranjananushka90@gmail.com>
2025-03-01 21:17:07 +0530

    Header

--- FoodApp/src/App.jsx
+++ FoodApp/src/App.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
-import Navbar from './components/Navbar/Navbar';
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import {Routes,Route} from 'react-router-dom'
+import Home from './screens/Home/Home'
+import Cart from './screens/Cart/Cart'
+import PlaceOrder from './screens/PlaceOrder/PlaceOrder'
 
 const App = () => {
   return (
-    <div className="App">
-      <Navbar />
+    <div className='app'>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/cart" element={<Cart/>}></Route>
+        <Route path="/order" element={<PlaceOrder/>}></Route>
+      </Routes>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App

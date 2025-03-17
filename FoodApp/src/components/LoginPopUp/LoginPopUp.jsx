bcbcac16ace6e3e237afea148bc884caba1bd019
ranjananushkacsv <ranjananushka90@gmail.com>
2025-03-17 21:03:28 +0530

    updated

--- FoodApp/src/components/LoginPopUp/LoginPopUp.jsx
+++ FoodApp/src/components/LoginPopUp/LoginPopUp.jsx
@@ -1,45 +1,37 @@
-import { useState } from "react";
-import "./LoginPopUp.css";
-import { assets } from "../../assets/assets";
+import {useState} from 'react'
+import './LoginPopUp.css'
+import {assets} from '../../assets/assets'
 
-const LoginPopUp = ({ setShowLogin }) => {
-  const [curState, setCurState] = useState("Sign Up");
-
+const LoginPopUp = ({setShowLogin}) => {
+    
+    const [curState,setCurState] = useState("Sign Up")
   return (
-    <div className="login-popup" onClick={() => setShowLogin(false)}>
-      <form className="login-popup-container" onClick={(e) => e.stopPropagation()}>
-        <div className="login-popup-title">
-          <h2>{curState}</h2>
-          <img onClick={() => setShowLogin(false)} src={assets.cross_icon} alt="Close" />
-        </div>
-
-        <div className="login-popup-inputs">
-          {curState === "Sign Up" && <input type="text" placeholder="Your Name" required />}
-          <input type="email" placeholder="Your Email" required />
-          <input type="password" placeholder="Password" required />
-        </div>
-
-        <button type="submit">{curState === "Sign Up" ? "Create Account" : "Log In"}</button>
-
-        <div className="login-popup-condition">
-          <input type="checkbox" required />
-          <p>{curState === "Sign Up" ? "I agree to the Terms & Conditions" : "Remember Me"}</p>
-        </div>
-
-        <p className="toggle-form">
-          {curState === "Sign Up" ? (
-            <>
-              Already have an account? <span onClick={() => setCurState("Log In")}>Log In</span>
-            </>
-          ) : (
-            <>
-              Don't have an account? <span onClick={() => setCurState("Sign Up")}>Sign Up</span>
-            </>
-          )}
-        </p>
-      </form>
+    <div className='login-popup'>
+        <form className='login-popup-container'>
+            <div className="login-popup-title">
+                <h2>{curState}</h2>
+                <img onClick={()=>setShowLogin(false)} src={assets.cross_icon} alt="" />
+            </div>
+            <div className="login-popup-inputs">
+                {curState!=="Log In" && <input type="text" placeholder='Your Name' required/>}
+                <input type="email" placeholder='Your Email' required/>
+                <input type="password" placeholder='Password' required/>
+                <button className='btn'>{curState==="Sign Up"?"Create Account":"Log In"}</button>
+            </div>
+            <div className="login-popup-condition">
+                <input type="checkbox" required/>
+                <p>
+                    By continuing, I agree to the terms & privacy policy
+                </p>
+            </div>
+            {
+                curState==="Log In" 
+                ? <p>Create a new account? <span onClick={()=>setCurState("Sign Up")}>Click here</span></p>
+                : <p>Already have an account? <span onClick={()=>setCurState("Log In")}>Log In</span></p>
+            }
+        </form>
     </div>
-  );
-};
+  )
+}
 
-export default LoginPopUp;
+export default LoginPopUp

0fea99664b59b7732cfdbfe54626ce0511ff4755
ranjananushkacsv <ranjananushka90@gmail.com>
2025-03-19 21:25:49 +0530

    Routed pages

--- adminFrontEnd/src/App.jsx
+++ adminFrontEnd/src/App.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Sidebar from './components/Sidebar/Sidebar'
+import Add from './screens/Add/Add'
+import List from './screens/List/List'
+import Orders from './screens/Orders/Orders'
+import {Routes, Route} from 'react-router-dom'
+import './App.css'
 const App = () => {
   return (
-    <div>
+    <div className='app'>
       <Navbar />
       <hr />
       <div className='app-content'>
         <Sidebar />
+        <Routes>
+          <Route path='/' element={<Add />}></Route>
+          <Route path='/add' element={<Add />}></Route>
+          <Route path='/list' element={<List />}></Route>
+          <Route path='/orders' element={<Orders />}></Route>
+
+        </Routes>
+
       </div>
     </div>
   )
--- adminFrontEnd/src/components/Sidebar/Sidebar.jsx
+++ adminFrontEnd/src/components/Sidebar/Sidebar.jsx
@@ -1,20 +1,24 @@
 import React from 'react'
 import './Sidebar.css'
 import { assets } from '../../assets/assets'
+import { NavLink } from 'react-router-dom'
+
 const Sidebar = () => {
   return (
-    <div className ="sidebar">
-      <div className="sidebar-option">
-        <img src ={assets.add_icon} alt=""/>
-        <p>Add Items</p>
-        </div>
-      <div className="sidebar-option">
-      <img src ={assets.list_icon} alt=""/>
-      <p>List Items</p>
-      </div>
-      <div className="sidebar-option">
-      <img src ={assets.order_icon} alt=""/>
-      <p>Orders</p>
+    <div className='sidebar'>
+      <div className="sidebar-options">
+        <NavLink to='/add' className="sidebar-option">
+          <img src={assets.add_icon} className='add-icon'/>
+          <p>Add Items</p>
+        </NavLink>
+        <NavLink to='/list' className="sidebar-option ">
+          <img src={assets.list_icon} className='list-icon'/>
+          <p>List Items</p>
+        </NavLink>
+        <NavLink to='/orders' className="sidebar-option">
+          <img src={assets.order_icon} className='order-icon' />
+          <p>Orders</p>
+        </NavLink>
       </div>
     </div>
   )
